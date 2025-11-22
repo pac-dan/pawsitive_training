cadaed53f82ea6df530b36c986f54bf0c1844279
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-22 19:38:08 +0200

    Fix model storage check script

--- check_model_storage.py
+++ check_model_storage.py
@@ -7,8 +7,11 @@ from training.models import Training
 from django.core.files.storage import default_storage
 
 print("Default storage:", type(default_storage).__name__)
-print("Training.video_file.storage:", type(Training.video_file.storage).__name__)
-print("Training.thumbnail.storage:", type(Training.thumbnail.storage).__name__)
+
+# Create a training instance to check field storage
+training = Training()
+print("Training instance video_file field storage:", type(training.video_file.storage).__name__)
+print("Training instance thumbnail field storage:", type(training.thumbnail.storage).__name__)
 
 # Check if they are the same object
-print("Same storage object?", Training.video_file.storage is default_storage)
+print("Same storage object?", training.video_file.storage is default_storage)
