ee34888937426e9bb3b29a12efcbb4733f58a63a
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-22 18:29:30 +0200

    Fix check script Django setup

--- check_training.py
+++ check_training.py
@@ -1,3 +1,7 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawsitive_training.settings')
+import django
+django.setup()
 from training.models import Training
 for t in Training.objects.order_by('-pk')[:3]:
     print(t.pk, t.title, t.video_file.name, bool(t.video_file.name))
