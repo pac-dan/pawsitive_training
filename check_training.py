84687e58835fcb07f9ec5ea2f5d9520690ed6123
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-22 20:03:55 +0200

    Fix subscription activation: add webhook logging and fallback activation in success view

--- check_training.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawsitive_training.settings')
-import django
-django.setup()
-from training.models import Training
-for t in Training.objects.order_by('-pk')[:3]:
-    print(t.pk, t.title, t.video_file.name, bool(t.video_file.name))
