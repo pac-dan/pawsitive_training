71b2cb41e057212f192d99c2fd449185555a9757
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-22 18:13:39 +0200

    Revert to default S3Boto3Storage backend with ContentDisposition

--- pawsitive_training/settings.py
+++ pawsitive_training/settings.py
@@ -208,7 +208,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
     # AWS S3 configuration for media files
-    DEFAULT_FILE_STORAGE = 'pawsitive_training.custom_storages.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
@@ -216,6 +216,7 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
+        'ContentDisposition': 'inline',
     }
     AWS_DEFAULT_ACL = 'public-read'
     AWS_QUERYSTRING_AUTH = False
