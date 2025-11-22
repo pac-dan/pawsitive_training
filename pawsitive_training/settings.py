ce988bf943e116499181575e11f592503d9e9fc1
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-22 15:29:07 +0200

    Revert "Make AWS S3 configuration optional for deployment"
    This reverts commit 308746eac2fb8c682d4a90449329389c995274c1.

--- pawsitive_training/settings.py
+++ pawsitive_training/settings.py
@@ -208,26 +208,23 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
-    # AWS S3 configuration for media files (optional)
-    USE_S3 = env.bool('USE_S3', default=False)
-    if USE_S3:
-        DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-        AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default='')
-        AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default='')
-        AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default='')
-        AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='eu-north-1')
-        if AWS_STORAGE_BUCKET_NAME:
-            AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-            AWS_S3_OBJECT_PARAMETERS = {
-                'CacheControl': 'max-age=86400',
-                'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-            }
-            AWS_DEFAULT_ACL = 'public-read'
-            AWS_QUERYSTRING_AUTH = False
-            AWS_S3_FILE_OVERWRITE = False
-            AWS_S3_ADDRESSING_STYLE = 'virtual'
-            AWS_S3_SIGNATURE_VERSION = 's3v4'
-            MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    # AWS S3 configuration for media files
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='eu-north-1')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    }
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_ADDRESSING_STYLE = 'virtual'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
