bc5848b8997f0c2a22c1af9c1504b170c9265afe
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-26 15:00:51 +0200

    Fix profile image bug: Remove default.jpg, allow null images to properly display placeholder icon

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     """
     Model to store user profiles.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(blank=True, null=True, upload_to='profile_pics')
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
