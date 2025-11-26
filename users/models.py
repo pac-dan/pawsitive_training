724f4c9b35968fea0ed3bd6cf830da708d73ea46
Daniel Johnston <djjoshnton98@hotmail.com>
2025-11-26 15:18:06 +0200

    Fix S3 storage for profile images: Add explicit S3Boto3Storage backend

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class Profile(models.Model):
@@ -7,7 +8,12 @@ class Profile(models.Model):
     Model to store user profiles.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, null=True, upload_to='profile_pics')
+    image = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='profile_pics',
+        storage=S3Boto3Storage()
+    )
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
